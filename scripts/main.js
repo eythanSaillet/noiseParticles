855f56d296bc824e095af463c7ef827282273e22
Eythan <eythan.saillet@gmail.com>
2020-01-29 00:02:21 +0100

    :sparkles: linking vectors directions to 3D perlin noise

--- scripts/main.js
+++ scripts/main.js
@@ -2,26 +2,29 @@ function setup()
 {
     createCanvas(700, 700).parent('canvasContainer')
     angleMode(DEGREES)
+    frameRate(60)
+
+    system.init()
 }
 
 class Particle
 {
-    constructor(posX, posY, speedX, speedY)
+    constructor(posX, posY, velX, velY)
     {
         this.posX = posX
         this.posY = posY
-        this.speedX = speedX
-        this.speedY = speedY
+        this.velX = velX
+        this.velY = velY
     }
 }
 
 class Vector
 {
-    constructor(posX, posY, dir, accX, accY)
+    constructor(posX, posY, accX, accY)
     {
         this.posX = posX
         this.posY = posY
-        this.dir = Math.floor(Math.random() * 360)
+        this.dir
         this.accX = accX
         this.accY = accY
     }
@@ -29,50 +32,61 @@ class Vector
 
 let system = 
 {
+    // SYSTEM PROPERTIES
     canvasWidth : 700,
     canvasHeight : 700,
     vectorsGridWidth : 50,
     vectorsGridHeight : 50,
-    vectorSize : 150,
+    vectorSize : 10,
 
     vectorsMatrix : [],
     particlesMatrix : [],
 
+    // NOISE PROPERTIES
+    noiseScaleXY : 0.2,
+    noiseScaleZ : 0.01,
+    zoff : 0,
+
+    // IF NEEDED LATER
     mousePosX : 0,
     mousePosY : 0,
 
     init()
     {
+        // CREATING A MATRIX OF VECTORS ACCORDING TO PROPERTIES OF THE SYSTEM
         const stepX = this.canvasWidth / this.vectorsGridWidth
         const stepY = this.canvasHeight / this.vectorsGridHeight
-
         for (let i = 0; i < this.vectorsGridHeight; i++)
         {
             const tab = []
             for (let j = 0; j < this.vectorsGridWidth; j++)
             {
-                tab.push(new Vector(i * stepY, j * stepX, 0, 0, 0))
+                tab.push(new Vector(i * stepY + stepY / 2, j * stepX + stepY / 2, 0, 0))
             }
             this.vectorsMatrix.push(tab)
         }
         console.log(this.vectorsMatrix)
     },
 
-    drawVectors()
+    defineVectors()
     {
         for (let i = 0; i < this.vectorsMatrix.length; i++)
         {
             for (let j = 0; j < this.vectorsMatrix[i].length; j++)
             {
+                this.vectorsMatrix[i][j].dir = noise(i * this.noiseScaleXY, j * this.noiseScaleXY, this.zoff * this.noiseScaleZ) * 720
+
+                // DRAWING VECTORS
                 const x = cos(this.vectorsMatrix[i][j].dir) * this.vectorSize
                 const y = sin(this.vectorsMatrix[i][j].dir) * this.vectorSize
                 line(this.vectorsMatrix[i][j].posX, this.vectorsMatrix[i][j].posY, this.vectorsMatrix[i][j].posX + x, this.vectorsMatrix[i][j].posY + y)
             }
         }
+        this.zoff++
     },
 }
-system.init()
 
+// IF NEEDED LATER
 function mouseMoved(event)
 {
     system.mousePosX = event.offsetX
@@ -82,5 +96,5 @@ function mouseMoved(event)
 function draw()
 {
     clear()
-    system.drawVectors()
+    system.defineVectors()
 }
